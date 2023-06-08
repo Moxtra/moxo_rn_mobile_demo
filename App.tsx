019cc278a4b0890071d98ce1881c212ef670d70e
john.hu <john.hu@moxo.com>
2023-06-08 15:47:48 +0800

    1. For home page

--- App.tsx
+++ App.tsx
@@ -6,10 +6,8 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
@@ -17,43 +15,9 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import { FAB, Image } from '@rneui/themed';
+import Colors from './src/components/Colors';
+import MoxoHeader from './src/components/MoxoHeader';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -63,56 +27,58 @@ function App(): JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={{
+      backgroundColor: isDarkMode ? Colors.black : Colors.white,
+      flex:1,
+    }}>
+      <View style={{
+      backgroundColor: isDarkMode ? Colors.black : Colors.white,
+      flex:1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+        {/* <MoxoHeader/>
+         */}
+        <Text
+        style={[
+            styles.text,
+            {
+            color: isDarkMode ? Colors.white : Colors.black,
+            },
+        ]}>
+        Welcome to
+        {'\n'}
+        Moxo!
+        </Text>
+        <FAB style={styles.fab}>
+        <Image
+            style={styles.icon}
+            source={ require('./src/assets/logo.png') }
+            containerStyle={styles.icon}> 
+        </Image>
+      </FAB>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
+  text: {
+    fontSize: 40,
     fontWeight: '700',
+    textAlign: 'center',
   },
+  fab: {
+    position: 'absolute',
+    right:30,
+    bottom:60
+  },
+  icon: {
+    position: 'absolute',
+    width: 56,
+    height: 56,
+    flex:1
+  }
 });
 
 export default App;
